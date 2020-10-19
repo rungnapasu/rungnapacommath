de417d6ef35003a3df0f428635486007c9122431
rungnapa <rungnapa.su.61@ubu.ac.th>
2020-10-19 19:43:01 +0700

    Rungnapa sutapak

--- fontend/src/views/Main/index.js
+++ fontend/src/views/Main/index.js
@@ -20,7 +20,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Rungmapa Website
+        Rungnapa Website
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
