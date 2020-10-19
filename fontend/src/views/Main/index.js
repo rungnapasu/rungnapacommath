3243d56d6cb29b4054713db59f095491268eddf6
rungnapa <rungnapa.su.61@ubu.ac.th>
2020-10-19 19:27:35 +0700

    final

--- fontend/src/views/Main/index.js
+++ fontend/src/views/Main/index.js
@@ -20,7 +20,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Numnim Website
+        Rungmapa Website
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
